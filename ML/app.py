be87724b8878d41671cce2b05d17b235ccedaae5
ItsSiddharth <siddharth.menon2018@vitstudent.ac.in>
2020-10-11 06:20:36 +0530

    Initial Commit

---
+++ ML/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import pickle
+import numpy as np
+from spider import trade_spider
+
+
+app = Flask(__name__)
+
+
+def load_model():
+	pkl_filename = "forest.pkl"
+	with open(pkl_filename, 'rb') as file:
+		pickle_model = pickle.load(file)
+
+	return pickle_model
+  
+@app.route("/", methods=["GET", "POST"])
+def index():
+	return {"Home_Page" : "Temporary placeholder"}
+
+@app.route("/model", methods=["GET", "POST"])
+def model_infer():
+	data = request.json
+	print(data)
+	value = list([list(data['input'])])
+	output = model.predict(value)
+	return {"output" : output}
+
+@app.route("/spider", methods=["GET", "POST"])
+def spider():
+	trade_spider()
+	return {"Result" : 1}
+
+if __name__ == "__main__":
+	model = load_model()
+	app.run(debug=True)
